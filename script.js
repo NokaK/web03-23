150ab440028367c0fbea0d6284d63b4e8509e6da
NokaK <nokakipiani93@gmail.com>
2023-05-17 21:33:22 +0400

    add variables

---
+++ script.js
@@ -0,0 +1,41 @@
+var firstName;
+var lastName = "Doe";
+var age = 35;
+var married = true;
+
+// console.log(
+//   "my name is " +
+//     firstName +
+//     " " +
+//     lastName +
+//     " and I am " +
+//     age +
+//     " years old."
+// );
+// console.log("I am married: " + married);
+
+// if(firstName !== undefined) {
+
+// }
+
+// console.log(`my name is  ${firstName}  ${lastName} and I am ${age} years old.`);
+// console.log(`I am married: ${married}`);
+
+var person = null;
+person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 35,
+  married: true,
+};
+
+console.log(
+  `my name is  ${person.firstName}  ${person.lastName} and I am ${person.age} years old.`
+);
+
+let x = "14";
+let c = 14;
+let y = 5;
+let z = 10;
+
+console.log(y + x + z + c);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+console.log(
+//   "my name is " +
+//     firstName +
+//     " " +
+//     lastName +
+//     " and I am " +
+//     age +
+//     " years old."
+// );
